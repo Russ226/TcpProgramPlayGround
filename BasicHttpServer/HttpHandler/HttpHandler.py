d460a1d38ac68fb95ed24cd9d3010f1a92f3a8d8
Ruslan Radetskiy <russ2510@gmail.com>
2024-08-16 22:02:44 -0400

    bug fixes

--- BasicHttpServer/HttpHandler/HttpHandler.py
+++ BasicHttpServer/HttpHandler/HttpHandler.py
@@ -6,6 +6,16 @@ sys.path.append(os.getcwd())
 from BasicHttpServer.Models.HttpRequest import HttpRequest
 from BasicHttpServer.Models.HttpResponse import HttpResponse
 
+CONTENTTYPES = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'js': 'text/js',
+    'png': 'image/png',
+    'jpeg': 'image/jpeg',
+    'tiff': 'image/tiff'
+    
+}
+
 class HttpHandler:
     def __init__(self, req: HttpRequest) -> None:
         self.request: HttpRequest = req
@@ -22,6 +32,27 @@ class HttpHandler:
                     retBody = readFile.read()
                     
                     return HttpResponse.createHttpResponse(code = 200, body = retBody, contentType = 'text/html')
+                
+        if os.path.isdir(os.path.join(self.workingDir, self.request.route)):
+            indexHtml = os.path.join(self.workingDir, self.request.route, 'index.html')
+            
+            if os.path.exists(indexHtml):
+                with open(indexHtml, 'rb') as readFile:
+                    retBody = readFile.read()
+                    
+                    return HttpResponse.createHttpResponse(code = 200, body = retBody, contentType = 'text/html')
+                
+        filepath = self.workingDir + self.request.route.replace('/', '\\')  
+        if os.path.isfile(filepath) and os.path.exists(filepath):
+            
+            with open(filepath, 'rb') as readFile:
+                retBody = readFile.read()
+                filetype = filepath[-3:]
+                
+                if filetype in CONTENTTYPES:
+                    return HttpResponse.createHttpResponse(code = 200, body = retBody, contentType = CONTENTTYPES[filetype])
+                
+                return HttpResponse.createHttpResponse(code = 200, body = retBody)
             
         return HttpResponse.createHttpResponse(code = 404)
             
